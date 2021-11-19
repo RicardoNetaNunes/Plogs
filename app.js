16e618cd9a704aea62559811e95ed8b5e5f1e86f
Ricardo Nunes <ricardo.agn.nunes@gmail.com>
2021-11-19 14:37:36 +0000

    display map

--- app.js
+++ app.js
@@ -46,6 +46,9 @@ app.use(session({
 const index = require("./routes/index");
 app.use("/", index);
 
+const searchRoutes = require('./routes/search.routes')
+app.use("/", searchRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
