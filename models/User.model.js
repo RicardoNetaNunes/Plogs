cc0d585cf3fd9fbde632fbb67b46b54483cdb8b0
Ines Cruz <inesmncruz@gmail.com>
2021-11-20 13:53:31 +0000

    loginworking

--- models/User.model.js
+++ models/User.model.js
@@ -12,7 +12,7 @@ const userSchema = new Schema({
   },
   email: {
     type: String,
-    required: true
+    //required: true (have to take it off, bc if true the user has to enter the email when logging in and we just ask for the username and password)
   },
 
   placesAdded: [{ type: Schema.Types.ObjectId, ref: 'place' }],
