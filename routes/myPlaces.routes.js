b192ee962f3bd542529bbc6cc4f133b28a899b86
Ricardo Nunes <ricardo.agn.nunes@gmail.com>
2021-11-24 10:15:00 +0000

    final stages

--- routes/myPlaces.routes.js
+++ routes/myPlaces.routes.js
@@ -7,25 +7,41 @@ const router = require("express").Router();
 //USER CAN EDIT OR DELETE PLACES ADDED BEFORE
 
 //EDIT
-router.get('/myPlaces/edit', (req, res, next) => {
-        res.render('places/edit.hbs')
+router.get('/myPlaces/:placesAddedId/edit', (req, res, next) => {
+    const {placesAddedId} = req.params 
+    //console.log('aaaaaaaaaaaaaiiiiiiiiiiiiiii' + placesAddedId)  
+    res.render('places/edit.hbs', {placesAddedId})
   });
 
-router.post('/myPlaces/:placesAddedId/edit', (req, res, next) => {
+router.post('/myPlaces/:placesAddedId/edit',uploader.single("image"), (req, res, next) => {
+
+
     const {placesAddedId} = req.params
-    console.log(placesAddedId)
-    Places.findByIdAndUpdate({placesAddedId}, req.body)
+    console.log('hiiiiiiii' + placesAddedId)
+    const {latitude, longitude, place, description} = req.body;
+    //console.log(latitude);
+    let image
+    if (!req.file){
+        image = '/images/default.jpg'
+    }
+    else {
+        image = req.file.path
+    }
+
+    Places.findByIdAndUpdate(placesAddedId, {latitude, longitude, place, description, image})
     .then(() => {
         res.redirect('/profile')
+        console.log('helppppppppppppppp')
     })
     .catch(() => {
-        next('Place not edited') //it's coming here :(
+        next('Place not edited') 
     })
   });
   
 //DELETE
 router.post('/myPlaces/:placesAddedId/delete', (req, res, next) => {
     const {placesAddedId} = req.params
+    console.log(placesAddedId)
     Places.findByIdAndDelete(placesAddedId)
     .then(() => {
         res.redirect('/profile')
